3ed11e6ac4e81afe59406b8a4dd1f3aaa92dfca2
Orlando21Lara <orlando21lara@gmail.com>
2023-12-04 00:01:36 -0500

    Lab4: RRT gets 8.96/10 by sampling in configuration space

--- lib/rrt.py
+++ lib/rrt.py
@@ -28,11 +28,14 @@ class Node:
 
 class RRTConnect:
     def __init__(self, map, start, goal):
+        self.lower = np.array([-2.8973,-1.7628,-2.8973,-3.0718,-2.8973,-0.0175,-2.8973])
+        self.upper = np.array([2.8973,1.7628,2.8973,-0.0698,2.8973,3.7525,2.8973])
+
         self.fk = FK()
         self.ik = IK()
 
         self.map = map
-        self.map_boundary = np.array([-1.0, -1.0, 0.1, 1.0, 1.0, 1.5])     # [xmin, ymin, zmin, xmax, ymax, zmax]
+        self.map_boundary = np.array([-1.0, -1.0, 0.05, 1.0, 1.0, 1.5])     # [xmin, ymin, zmin, xmax, ymax, zmax]
 
         self.startTree = []
         self.start_node = self.createNode(q=start)
@@ -43,9 +46,11 @@ class RRTConnect:
         self.goalTree.append(self.goal_node)
 
         self.reached = False
-        self.max_iter = 1000
-        self.step_size = 0.1            # meters
-        self.sphere_radius = 0.2        # meters
+        self.max_iter = 10000
+        # self.step_size = 0.1              # meters
+        self.step_size = 2.0                # rad
+        self.arm_sphere_radius = 0.05       # meters
+        self.obs_sphere_radius = 0.20        # meters
     
     def createNode(self, position=None, q=None, parent=None):
         if q is not None:
@@ -77,22 +82,27 @@ class RRTConnect:
             nearest_start_tree_node = self.nearestNodeInTree(sample_node, start_tree=True)
             connected_sample_to_start_tree, intermediate_nodes = self.connectNodes(nearest_start_tree_node, sample_node)
             if connected_sample_to_start_tree:
-                self.startTree + intermediate_nodes
+                # self.startTree + intermediate_nodes
                 sample_node.parent = nearest_start_tree_node
                 self.startTree.append(sample_node)
 
             # Get the node in the goal tree that is closest to the sample node and try to connect them
             nearest_goal_tree_node = self.nearestNodeInTree(sample_node, start_tree=False)
-            connected_sample_to_goal_tree, intermediate_nodes = self.connectNodes(sample_node, nearest_goal_tree_node)
+            connected_sample_to_goal_tree, intermediate_nodes = self.connectNodes(nearest_goal_tree_node, sample_node)
             if connected_sample_to_goal_tree:
-                self.goalTree + intermediate_nodes
+                # self.goalTree + intermediate_nodes
+                sample_node_copy = deepcopy(sample_node)
+                sample_node_copy.parent = nearest_goal_tree_node
+                self.goalTree.append(sample_node_copy)
             
             if connected_sample_to_start_tree and connected_sample_to_goal_tree:
                 self.reached = True
-                return self.getFinalPath()
+                return self.getFinalPath(sample_node, sample_node_copy)
 
             idx += 1
             print("Iteration: ", idx)
+            print("\tStart Tree Size: ", len(self.startTree))
+            print("\tGoal Tree Size: ", len(self.goalTree))
 
         # Could not find a path
         return []
@@ -101,15 +111,16 @@ class RRTConnect:
         # Sample random point in free space (x, y, z) taking into account the boundary
         sample_is_in_collision = True
         while(sample_is_in_collision):
-            x = random.uniform(self.map_boundary[0], self.map_boundary[3])
-            y = random.uniform(self.map_boundary[1], self.map_boundary[4])
-            z = random.uniform(self.map_boundary[2], self.map_boundary[5])
-            for obs in self.map.obstacles:
-                if detectCollision.pointBoxCollision([x, y, z], obs):
-                    continue
+            # x = random.uniform(self.map_boundary[0], self.map_boundary[3])
+            # y = random.uniform(self.map_boundary[1], self.map_boundary[4])
+            # z = random.uniform(self.map_boundary[2], self.map_boundary[5])
+            # for obs in self.map.obstacles:
+            #     if detectCollision.pointBoxCollision([x, y, z], obs):
+            #         continue
+            sample_q = np.random.uniform(self.lower, self.upper)
                     
             # The point is not in collision with any of the obstacles now check if the arm is in collision
-            sample_node = self.createNode(position=[x, y, z], parent=None)
+            sample_node = self.createNode(q=sample_q, parent=None)
             if sample_node.is_valid:
                 if not self.armIsInCollision(sample_node):
                     sample_is_in_collision = False
@@ -126,7 +137,8 @@ class RRTConnect:
         min_dist = float('inf')
         nearest_node = None
         for tree_node in tree:
-            dist = np.linalg.norm(tree_node.ef_pose[:3,3] - node.ef_pose[:3,3])
+            # dist = np.linalg.norm(tree_node.ef_pose[:3,3] - node.ef_pose[:3,3])
+            dist = np.linalg.norm(tree_node.q - node.q)
             if dist < min_dist:
                 min_dist = dist
                 nearest_node = tree_node
@@ -140,56 +152,83 @@ class RRTConnect:
         return true if there was no collision along with the intermediate nodes else return false
         and an empty list
         """
-        parent_position = parent_node.ef_pose[:3,3]
-        child_position = child_node.ef_pose[:3,3]
-        dist = np.linalg.norm(child_position - parent_position)
-        direction = (child_position - parent_position) / dist
+        dist = np.linalg.norm(child_node.q - parent_node.q)
+        if dist < self.step_size:
+            return True, []
+        else:
+            return False, []
+        # parent_position = parent_node.ef_pose[:3,3]
+        # child_position = child_node.ef_pose[:3,3]
+        # dist = np.linalg.norm(child_position - parent_position)
+        # direction = (child_position - parent_position) / dist
 
-        dist_traveled = 0.0
-        curr_position = parent_position
-        curr_parent_node = parent_node
-        intermediate_nodes = []
-        while(dist_traveled < dist):
-            curr_position += direction * self.step_size
-            curr_node = self.createNode(position=curr_position, parent=curr_parent_node)
-            if not curr_node.is_valid:
-                return False, []
-            elif self.armIsInCollision(curr_node):
-                return False, []
-            else:
-                intermediate_nodes.append(curr_node)
-                curr_parent_node = curr_node
-                dist_traveled += self.step_size
+        # dist_traveled = 0.0
+        # curr_position = parent_position
+        # curr_parent_node = parent_node
+        # intermediate_nodes = []
+        # while(dist_traveled < dist):
+        #     curr_position += direction * self.step_size
+        #     curr_node = self.createNode(position=curr_position, parent=curr_parent_node)
+        #     if not curr_node.is_valid:
+        #         return False, []
+        #     elif self.armIsInCollision(curr_node):
+        #         return False, []
+        #     else:
+        #         intermediate_nodes.append(curr_node)
+        #         curr_parent_node = curr_node
+        #         dist_traveled += self.step_size
         
-        return True, intermediate_nodes
+        # return True, intermediate_nodes
 
     def armIsInCollision(self, node):
         # Check if the arm is in self collision by checking if any of the joints are in collision with each other
         num_joints = node.joint_positions.shape[0]
         for i in range(num_joints):
             for j in range(i + 1, num_joints):
-                if detectCollision.sphereSphereCollision(sphere1_pos=node.joint_positions[i], sphere2_pos=node.joint_positions[j], sphere_r=self.sphere_radius):
+                if detectCollision.sphereSphereCollision(sphere1_pos=node.joint_positions[i], sphere2_pos=node.joint_positions[j], sphere_r=self.arm_sphere_radius):
                     return True
 
         # Check if the arm is in collision with any of the obstacles
         for obs in self.map.obstacles:
             # Check if any of the joints are in collision with the obstacle
-            for i in range(node.joint_pos.shape[0]):
-                if detectCollision.sphereBoxCollision(node.joint_positions[i], self.sphere_radius, obs):
+            for i in range(node.joint_positions.shape[0]):
+                if detectCollision.sphereBoxCollision(node.joint_positions[i], self.obs_sphere_radius, obs):
                     return True
             # Check if the end effector is in collision with the obstacle
             if detectCollision.pointBoxCollision(node.ef_pose[:3,3], obs):
                 return True
+        
+        # Check if the arm is in collision with the floor
+        for i in range(1, node.joint_positions.shape[0]-1):
+            if detectCollision.sphereBoxCollision(node.joint_positions[i], self.obs_sphere_radius , np.array([-1.0, -1.0, -1.0, 1.0, 1.0, 0.00])):
+                return True
 
-    def getFinalPath(self):
+    def getFinalPath(self, start_tree_final_node, goal_tree_final_node):
+        # Get the path from the start node to the goal node
+        # path = []
+        # curr_node = self.goal_node
+        # while curr_node is not None:
+        #     path.append(curr_node.q)
+        #     curr_node = curr_node.parent
+        
+        # path.reverse()
+        # return deepcopy(path)
+
         # Get the path from the start node to the goal node
         path = []
-        curr_node = self.goal_node
+        # Start from the start tree
+        curr_node = start_tree_final_node
         while curr_node is not None:
-            path.append(curr_node)
+            path.append(curr_node.q)
+            curr_node = curr_node.parent
+        path.reverse()
+
+        # Now append the path from the goal tree
+        curr_node = goal_tree_final_node
+        while curr_node is not None:
+            path.append(curr_node.q)
             curr_node = curr_node.parent
         
-        path.reverse()
         return deepcopy(path)
 
 def rrt(map, start, goal):
@@ -207,6 +246,8 @@ def rrt(map, start, goal):
     return path
 
 if __name__ == '__main__':
+    # set a constant seed for reproducibility
+    np.random.seed(42)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     print("Directory of this file: ", dir_path)
 
