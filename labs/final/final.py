bccc196cb9abab299476b70e4b4f1f8b5f010fbf
Orlando21Lara <orlando21lara@gmail.com>
2023-12-09 12:07:22 -0500

    FinalProject: WIP velocity IK

--- labs/final/final.py
+++ labs/final/final.py
@@ -14,6 +14,8 @@ from core.utils import time_in_seconds
 # Personal modules
 from lib.calculateFK import FK
 from lib.IK_position_null import IK
+from lib.IK_velocity_null import IK_velocity_null
+from lib.calcAngDiff import calcAngDiff
 
 # ROS modules
 import rospy
@@ -30,12 +32,196 @@ def show_pose(H, child_frame, parent_frame="world"):
         parent_frame
     )
 
+def rotvec_to_matrix(rotvec):
+    theta = np.linalg.norm(rotvec)
+    if theta < 1e-9:
+        return np.eye(3)
+
+    # Normalize to get rotation axis.
+    k = rotvec / theta
+    K = np.array([
+        [0, -k[2], k[1]],
+        [k[2], 0, -k[0]],
+        [-k[1], k[0], 0]
+    ])
+    R = np.eye(3) + np.sin(theta) * K + (1 - np.cos(theta)) * np.dot(K, K)
+    return R
+
+class TrapezoidalVelocityProfile():
+    def __init__(self, start_position, goal_position, v_max, a_max):
+        self.v_max = v_max
+        self.a_max = a_max
+        self.start_position = start_position
+        self.goal_position = goal_position
+        self.distance = np.linalg.norm(goal_position - start_position)
+        self.direction = (goal_position - start_position) / self.distance
+
+        self.t1 = self.v_max / self.a_max       # Time to reach max velocity
+        self.t2 = self.distance / self.v_max    # Time to stop traveling at max velocity and start decelerating
+
+        self.t_total = self.t1 + self.t2
+    
+    def getTrajectory(self, t):
+        if t < self.t1:
+            # Accelerating
+            des_pos = self.start_position + 0.5 * self.a_max * t**2 * self.direction    # self.a_max = self.v_max / self.t1
+            des_vel = self.a_max * t * self.direction
+            return des_pos, des_vel
+        elif t < self.t2:
+            # Traveling at max velocity
+            des_pos = self.start_position + self.v_max * (t - 0.5 * self.t1) * self.direction
+            des_vel = self.v_max * self.direction
+            return des_pos, des_vel
+        elif t < self.t_total:
+            # Decelerating
+            des_pos = self.start_position + self.direction * (-self.v_max * 0.5 / (self.t_total - self.t2) * (self.t2**2 + t**2) \
+                                                              + self.v_max / (self.t_total - self.t2) * self.t_total * t \
+                                                              - self.v_max * 0.5 * self.t1)
+            des_vel = self.direction * (self.v_max / (self.t_total - self.t2) * (self.t_total - t))
+            return des_pos, des_vel
+        else:
+            # Done
+            return self.goal_position, np.zeros(3)
+
+class TriangularVelocityProfile():
+    def __init__(self, start_position, goal_position, v_max, a_max):
+        self.v_max = v_max
+        self.a_max = a_max
+        self.start_position = start_position
+        self.goal_position = goal_position
+        self.distance = np.linalg.norm(goal_position - start_position)
+        self.direction = (goal_position - start_position) / self.distance
+
+        self.tp = np.sqrt(self.distance / self.a_max)   # Time to reach peak velocity
+        self.t_total = 2 * self.tp
+    
+    def getTrajectory(self, t):
+        v_top = self.a_max * self.tp
+
+        if t < self.tp:
+            # Accelerating
+            des_pos = self.start_position + 0.5 * self.a_max * t**2 * self.direction    # self.a_max = self.v_top / self.tp
+            des_vel = self.a_max * t * self.direction
+            return des_pos, des_vel
+        elif t < self.t_total:
+            # Decelerating
+            des_pos = self.start_position + self.direction * (v_top * 0.5 / (self.t_total - self.tp) * (self.tp**2 - t**2) \
+                                                              + v_top / (self.t_total - self.tp) * self.t_total * (t - self.tp) \
+                                                              + v_top * 0.5 * self.tp)
+            des_vel = self.direction * (v_top / (self.t_total - self.tp) * (self.t_total - t))
+            return des_pos, des_vel
+        else:
+            # Done
+            return self.goal_position, np.zeros(3)
+
+class VelocityController():
+    def __init__(self, fk, start_pose, goal_pose):
+        self.active = False
+
+        self.fk = fk
+        self.start_time = None
+        self.last_iteration_time = None
+
+        self.a_max = 1.0    # m/s^2
+        self.v_max = 1.0    # m/s
+
+        self.kp = 5.0       # Proportional gain for position
+        self.kr = 5.0       # Proportional gain for rotation
+        self.k0 = 1.0       # Proportional gain for secondary task
+
+        # Determine which velocity profile to use
+        self.start_pose = start_pose
+        self.goal_pose = goal_pose
+        start_position = start_pose[:3,3]
+        goal_position = goal_pose[:3,3]
+        self.distance = np.linalg.norm(goal_position - start_position)
+        if self.distance < self.v_max**2 / self.a_max:
+            self.trajectory = TriangularVelocityProfile(start_position, goal_position, self.v_max, self.a_max)
+        else:
+            self.trajectory = TrapezoidalVelocityProfile(start_position, goal_position, self.v_max, self.a_max)
+        
+        self.active = True
+
+        self.time_stamps = []
+        self.des_pos_over_time = []
+        self.des_vel_over_time = []
+        self.real_pos_over_time = []
+        self.commanded_vel_over_time = []
+        self.dq_over_time = []
+
+    def getSecondaryTask(self, q):
+        # This is an elbow up preference task
+        lower = np.array([-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973])
+        upper = np.array([2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973])
+
+        q_e = lower + (upper - lower) / 2
+
+        return -self.k0 * (q - q_e)
+
+    def followTrajectory(self, state, arm):
+        if self.active:
+            try:
+                if self.start_time is None:
+                    self.start_time = time_in_seconds()
+                    self.last_iteration_time = self.start_time
+                
+                curr_time = time_in_seconds()
+
+                t = curr_time - self.start_time
+
+                # Get desired trajectory
+                x_des, v_des = self.trajectory.getTrajectory(t)
+                if t > self.trajectory.t_total:
+                    # Done
+                    self.active = False
+                    return
+
+                # Get current end effector position
+                q = state['position']
+
+                _, T0e = self.fk.forward(q)
+
+                R_curr = (T0e[:3,:3])
+                x_curr = (T0e[0:3,3])
+
+                # Compute target velocities
+                v_tar = v_des + self.kp * (x_des - x_curr)
+                # w_tar = w_des + self.kr * calcAngDiff(R_des, R_curr).flatten()
+
+                b = self.getSecondaryTask(q)
+
+                # Velocity Inverse Kinematics
+                dq = IK_velocity_null(q, v_tar, np.zeros(3), b).flatten()
+
+                self.dt = curr_time - self.last_iteration_time
+                self.last_iteration_time = curr_time
+
+                new_q = q + self.dt * dq
+
+                # Store trajectory data
+                self.time_stamps.append(t)
+                self.real_pos_over_time.append(x_curr)
+                self.des_pos_over_time.append(x_des)
+                self.des_vel_over_time.append(v_des)
+                self.commanded_vel_over_time.append(v_tar)
+                self.dq_over_time.append(dq)
+
+                arm.set_joint_positions_velocities(new_q, dq, is_safe=True)
+
+            except rospy.exceptions.ROSException:
+                pass
+
 class BlockStacker:
-    def __init__(self, arm, detector):
-        self.arm = arm
-        self.detector = detector
+    def __init__(self, team):
         self.fk = FK()
         self.ik = IK()
+        self.team = team
+
+        self.detector = ObjectDetector()
+        self.vel_controller = None
+
+        callback = lambda state : self.stateCallback(state)
+        self.arm = ArmController(on_state_callback=callback)
 
         self.q_static_table_view = np.array([0, 0, 0, -pi/2, 0, pi/2, pi/4])
         self.H_world_static_table_view = self.fk.forward(self.q_static_table_view)[1]
@@ -54,6 +240,10 @@ class BlockStacker:
         self.static_blocks = []
         self.dynamic_blocks = []
     
+    def stateCallback(self, state):
+        if self.vel_controller is not None:
+            self.vel_controller.followTrajectory(state, self.arm)
+
     def changeAxis(self, T):
         largest_mag = 0
         vertical_axis = None
@@ -90,6 +280,37 @@ class BlockStacker:
             H_target[:3,1] = -H_target[:3,1]
 
         return H_target
+
+    def moveToPosition(self, goal_pose):
+        start_pose = self.fk.forward(self.arm.get_positions())[1]
+        self.vel_controller = VelocityController(self.fk, start_pose, goal_pose)
+        while(self.vel_controller.active):
+            pass
+        
+        self.plotTrajectory(self.vel_controller)
+        # Reset the velocity controller so that stateCallback does not try to follow the trajectory
+        self.vel_controller = None
+        
+    def plotTrajectory(self, vel_controller):
+        import matplotlib.pyplot as plt
+        fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+        fig.suptitle('Trajectory Tracking')
+
+        axs[0].plot(vel_controller.time_stamps, vel_controller.real_pos_over_time, label="Real")
+        axs[0].plot(vel_controller.time_stamps, vel_controller.des_pos_over_time, label="Desired")
+        axs[0].set_ylabel("Position (m)")
+        axs[0].legend()
+
+        axs[1].plot(vel_controller.time_stamps, vel_controller.des_vel_over_time, label="Desired")
+        axs[1].plot(vel_controller.time_stamps, vel_controller.commanded_vel_over_time, label="Commanded")
+        axs[1].set_ylabel("Velocity (m/s)")
+        axs[1].legend()
+
+        axs[2].plot(vel_controller.time_stamps, vel_controller.dq_over_time)
+        axs[2].set_ylabel("Joint Velocity (rad/s)")
+        axs[2].set_xlabel("Time (s)")
+
+        plt.show()
 
     def moveToStaticTableView(self):
         self.arm.safe_move_to_position(self.q_static_table_view)
@@ -152,6 +373,7 @@ class BlockStacker:
 
 
 def main():
+    np.set_printoptions(precision=4, suppress=True)
     try:
         team = rospy.get_param("team") # 'red' or 'blue'
     except KeyError:
@@ -159,11 +381,11 @@ def main():
         exit()
 
     rospy.init_node("team_script")
-    arm = ArmController()
-    detector = ObjectDetector()
+
+    block_stacker = BlockStacker(team)
 
     start_position = np.array([-0.01779206, -0.76012354,  0.01978261, -2.34205014, 0.02984053, 1.54119353+pi/2, 0.75344866])
-    arm.safe_move_to_position(start_position) # on your mark!
+    block_stacker.arm.safe_move_to_position(start_position) # on your mark!
 
     print("\n****************")
     if team == 'blue':
@@ -180,10 +402,22 @@ def main():
     Panda Block Stacking Challenge Code Begins Here
     ###############################################
     """
-    block_stacker = BlockStacker(arm, detector)
 
     block_stacker.moveToStaticTableView()
-    block_stacker.stackStaticBlocks()
+    print("Moved to static table view")
+    # Print current position
+    H_curr = block_stacker.fk.forward(block_stacker.arm.get_positions())[1]
+    print("Current pose:", H_curr)
+
+    H_target = deepcopy(H_curr)
+    H_target[2, 3] -= 0.2
+
+    print("Moving to target position:", H_target)
+    block_stacker.moveToPosition(H_target)
+    print("Done moving to target position")
+    new_H_curr = block_stacker.fk.forward(block_stacker.arm.get_positions())[1]
+    print("Current pose:", new_H_curr)
+    # block_stacker.stackStaticBlocks()
 
 
 if __name__ == "__main__":
